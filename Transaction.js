443fb8271ac64df98276797de41b19d716e722bb
oxhelium <quentincrto@gmail.com>
2022-12-04 14:55:40 +0100

    updated transactions

--- Transaction.js
+++ Transaction.js
@@ -1,12 +1,23 @@
 class Transaction {
-    constructor(inputUTXOs, outputUTXOs) {
-        this.inputUTXOs = inputUTXOs;
-        this.outputUTXOs = outputUTXOs;
+  constructor(inputUTXOs, outputUTXOs) {
+    this.inputUTXOs = inputUTXOs;
+    this.outputUTXOs = outputUTXOs;
+  }
+  execute() {
+    const anySpent = this.inputUTXOs.some((x) => x.spent);
+    if (anySpent) {
+      throw new Error("Cannot spend already spent UTXOs");
     }
-    execute() {
-        const anySpent = this.inputUTXOs.some((x) => x.spent);
-        if (anySpent) {
-            throw new Error ("Cannot spend already spent UTXOs");
-        }
+
+    const inputAmount = this.inputUTXOs.reduce((p, c) => {
+      return p + c.amount;
+    }, 0);
+    const outputAmount = thisd.outputUTXOs.reduce((p, c) => {
+      return p + c.amount;
+    }, 0);
+
+    if (inputAmount < outputAmount) {
+      throw new Error("Not enough funds");
     }
+  }
 }
